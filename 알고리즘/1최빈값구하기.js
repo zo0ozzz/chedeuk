e2b2a47e598d0256b7901bf1423efa16bf2b2d9b
zo0ozzz <ungbi@kakao.com>
2023-07-02 05:33:16 +0900

    230701

--- 알고리즘/1최빈값구하기.js
+++ 알고리즘/1최빈값구하기.js
@@ -12,43 +12,73 @@ const array = [1, 2, 3, 3, 3, 4];
 const answer = 3;
 
 // 정답
-// function solution(array) {
-//   if (array.length === 1) {
-//     return array[0];
-//   }
-
-//   const unique = [...new Set(array)];
-//   if (unique.length === 1) {
-//     return unique[0];
-//   }
-
-//   const newArray = unique.map((item) => {
-//     let count = 0;
-
-//     for (i = 0; i < array.length; i++) {
-//       if (item === array[i]) {
-//         count++;
-//       }
-//     }
-
-//     return [item, count];
-//   });
-
-//   const copy = [...newArray];
-
-//   copy.sort((first, second) => {
-//     return second[1] - first[1];
-//   });
-
-//   if (copy[0][1] === copy[1][1]) {
-//     return -1;
-//   } else {
-//     return copy[0][0];
-//   }
-// }
 
 function solution(array) {
-  return;
+  if (array.length === 1) {
+    return array[0];
+  }
+
+  const unique = [...new Set(array)];
+  if (unique.length === 1) {
+    return unique[0];
+  }
+
+  const newArray = unique.map((item) => {
+    let count = 0;
+
+    for (i = 0; i < array.length; i++) {
+      if (item === array[i]) {
+        count++;
+      }
+    }
+
+    return [item, count];
+  });
+
+  const copy = [...newArray];
+
+  copy.sort((first, second) => {
+    return second[1] - first[1];
+  });
+
+  if (copy[0][1] === copy[1][1]) {
+    return -1;
+  } else {
+    return copy[0][0];
+  }
 }
 
-console.log("최빈값 구하기: ", solution(array) === answer);
+// * 230701: 13분
+// - 기존엔 map을 사용했음.
+// - 이번엔 reduce로 시도.
+//  - reduce가 익숙치 않아서 시간이 좀 걸림.
+// - 알고리즘 문제는 답이 중요하니까 원본 배열이 바뀌어도 신경 쓰지 말자. 일단은 답이다.
+// - 배열 순회 메서드 안에서 또 배열을 순회하는 게 좀 깔끔하지 않아 보임. 다른 방법이 있을까?
+
+function solution230701(array) {
+  const unique = [...new Set(array)];
+
+  const elementAndCountArray = unique.reduce((acc, item) => {
+    let count = 0;
+
+    for (i = 0; i < array.length; i++) {
+      if (item === array[i]) {
+        count++;
+      }
+    }
+
+    acc = [...acc, [item, count]];
+
+    return acc;
+  }, []);
+
+  const sortedArray = elementAndCountArray.sort((a, b) => b[1] - a[1]);
+
+  if (sortedArray[0][1] === sortedArray[1][1]) {
+    return -1;
+  }
+
+  return sortedArray[0][0];
+}
+
+console.log("최빈값 구하기: ", solution230701(array) === answer);
--- 알고리즘/2분수의덧셈.js
+++ 알고리즘/2분수의덧셈.js
@@ -14,83 +14,157 @@ const denom2 = 4;
 
 const answer = [5, 4];
 
-// function solution(numer1, denom1, numer2, denom2) {
-//     let arr1 = [];
-//     let arr2 = [];
-
-//     for(i = 1; i <= denom1; i++) {
-//         if(denom1 % i === 0) {
-//             arr1.push(i)
-//         }
-
-//         continue;
-//     }
-
-//     for(i = 1; i <= denom2; i++) {
-//         if(denom2 % i === 0) {
-//             arr2.push(i)
-//         }
-
-//         continue;
-//     }
-
-//     const arr = arr1.filter(item => {
-//         for(i = 0; i < arr2.length; i++) {
-//             if(item === arr2[i]) {
-//                 return item;
-//             }
-//         }
-//     })
-
-//     console.log(arr1, arr2, arr)
-
-//     const copyArr = [...arr];
-//     const 분모최대공약수 = copyArr.sort().pop();
-
-//     const value1 = denom1 / 분모최대공약수;
-//     const value2 = denom2 / 분모최대공약수;
-
-//     const 분모최소공배수 = 분모최대공약수 * value1 * value2;
-
-//     const 분자환산 = numer1 * (분모최소공배수 / denom1) + numer2 * (분모최소공배수 / denom2);
-//     const 분모환산 = 분모최소공배수;
-
-//     // const reverseArr = [...arr].reverse();
-
-//     let newArr = []
-
-//     for(i = 1; i <= 분모최소공배수; i++) {
-//         if(분모최소공배수 % i === 0) {
-//             newArr.push(i)
-//         }
-
-//         continue;
-//     }
-
-//     const reverseNewArr = [...newArr].reverse();
-
-//     let result분자;
-//     let result분모;
-
-//     for(i = 0; i < reverseNewArr.length; i++) {
-//         const divider = reverseNewArr[i];
-
-//         if(분자환산 % divider === 0) {
-//             result분자 = 분자환산 / divider;
-//             result분모 = 분모환산 / divider;
-
-//             break;
-//         }
-//     }
-
-//     return [result분자, result분모]
-// }
-
 function solution(numer1, denom1, numer2, denom2) {
-  return;
+  let arr1 = [];
+  let arr2 = [];
+
+  for (i = 1; i <= denom1; i++) {
+    if (denom1 % i === 0) {
+      arr1.push(i);
+    }
+
+    continue;
+  }
+
+  for (i = 1; i <= denom2; i++) {
+    if (denom2 % i === 0) {
+      arr2.push(i);
+    }
+
+    continue;
+  }
+
+  const arr = arr1.filter((item) => {
+    for (i = 0; i < arr2.length; i++) {
+      if (item === arr2[i]) {
+        return item;
+      }
+    }
+  });
+
+  console.log(arr1, arr2, arr);
+
+  const copyArr = [...arr];
+  const 분모최대공약수 = copyArr.sort().pop();
+
+  const value1 = denom1 / 분모최대공약수;
+  const value2 = denom2 / 분모최대공약수;
+
+  const 분모최소공배수 = 분모최대공약수 * value1 * value2;
+
+  const 분자환산 =
+    numer1 * (분모최소공배수 / denom1) + numer2 * (분모최소공배수 / denom2);
+  const 분모환산 = 분모최소공배수;
+
+  // const reverseArr = [...arr].reverse();
+
+  let newArr = [];
+
+  for (i = 1; i <= 분모최소공배수; i++) {
+    if (분모최소공배수 % i === 0) {
+      newArr.push(i);
+    }
+
+    continue;
+  }
+
+  const reverseNewArr = [...newArr].reverse();
+
+  let result분자;
+  let result분모;
+
+  for (i = 0; i < reverseNewArr.length; i++) {
+    const divider = reverseNewArr[i];
+
+    if (분자환산 % divider === 0) {
+      result분자 = 분자환산 / divider;
+      result분모 = 분모환산 / divider;
+
+      break;
+    }
+  }
+
+  return [result분자, result분모];
+}
+
+// 230701: 35분
+// - 이번에도
+//   const parsedNumer1 = numer1 * divided2;
+//   const parsedNumer2 = numer2 * divided1;
+//   여기서 2, 1을 바꿔 썼다. 이게 왤케 헷갈릴까.
+// - 코드가 너무 긴 것 같다. 좀 더 간단하게 하는 방법이 있을까.
+// - 최대공약수, 최소공배수를 구하는 방식을 확실히 익혀보자.
+
+function solution230701(numer1, denom1, numer2, denom2) {
+  const 약수1 = [];
+  const 약수2 = [];
+
+  for (i = 1; i <= denom1; i++) {
+    if (denom1 % i === 0) {
+      약수1.push(i);
+    }
+  }
+
+  for (i = 1; i <= denom2; i++) {
+    if (denom2 % i === 0) {
+      약수2.push(i);
+    }
+  }
+
+  const common = 약수1.filter((item) => {
+    for (i = 0; i < 약수2.length; i++) {
+      if (item === 약수2[i]) {
+        return item;
+      }
+    }
+  });
+
+  const commonMax = common.sort((a, b) => b - a)[0];
+  const divided1 = denom1 / commonMax;
+  const divided2 = denom2 / commonMax;
+
+  // 분모
+  const denomMin = commonMax * divided1 * divided2;
+
+  const parsedNumer1 = numer1 * divided2;
+  const parsedNumer2 = numer2 * divided1;
+
+  // 분자
+  const sumNumer = parsedNumer1 + parsedNumer2;
+
+  const commonDenomMin = [];
+  const commonSumNuber = [];
+
+  for (i = 1; i <= denomMin; i++) {
+    if (denomMin % i === 0) {
+      commonDenomMin.unshift(i);
+    }
+  }
+
+  for (i = 1; i <= sumNumer; i++) {
+    if (sumNumer % i === 0) {
+      commonSumNuber.unshift(i);
+    }
+  }
+
+  let newCommonMax;
+
+  for (i = 0; i < commonDenomMin.length; i++) {
+    for (j = 0; j < commonSumNuber.length; j++) {
+      if (commonDenomMin[i] === commonSumNuber[j]) {
+        newCommonMax = commonDenomMin[i];
+      }
+    }
+  }
+
+  const resultNumer = sumNumer / newCommonMax;
+  const resultDenom = denomMin / newCommonMax;
+
+  return [resultNumer, resultDenom];
 }
 
 console.log(
   "분수의덧셈: ",
-  solution(numer1, denom1, numer2, denom2) === [5, 4]
+  solution230701(numer1, denom1, numer2, denom2)[0] === 5 &&
+    solution230701(numer1, denom1, numer2, denom2)[1] === 4
 );
