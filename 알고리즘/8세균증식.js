da32166486b161d1c4d558d6b0d3d08c9ec6d79d
[SW4/2반] 정웅비 <zo0qzzz@gmail.com>
2023-07-19 16:32:30 +0900

    230719

---
+++ 알고리즘/8세균증식.js
@@ -0,0 +1,37 @@
+// 세균 증식
+// https://school.programmers.co.kr/learn/courses/30/lessons/120910
+
+// 문제 설명
+// 어떤 세균은 1시간에 두배만큼 증식한다고 합니다. 처음 세균의 마리수 n과 경과한 시간 t가 매개변수로 주어질 때 t시간 후 세균의 수를 return하도록 solution 함수를 완성해주세요.
+
+// 제한사항
+// 1 ≤ n ≤ 10
+// 1 ≤ t ≤ 15
+
+// 조건 & 결괏값
+const n = 2;
+const t = 10;
+
+const answer = 2048;
+
+// 정답
+// - 목적
+// 	- Math.pow() 메서드
+function solution(n, t) {
+  return n * Math.pow(2, t);
+
+  //     let quantity = n;
+
+  //     for(i = 1; i <= t; i++) {
+  //         quantity *= 2;
+  //     }
+
+  //     return quantity;
+}
+
+// 230719
+function solution230719(n, t) {
+  return;
+}
+
+console.log(solution230719(n, t) === answer);
---
+++ 알고리즘/9문자열정렬하기(2).js
@@ -0,0 +1,41 @@
+// 문자열 정렬하기 (2)
+// https://school.programmers.co.kr/learn/courses/30/lessons/120911
+
+// 문제 설명
+// 영어 대소문자로 이루어진 문자열 my_string이 매개변수로 주어질 때, my_string을 모두 소문자로 바꾸고 알파벳 순서대로 정렬한 문자열을 return 하도록 solution 함수를 완성해보세요.
+
+// 제한사항
+// 0 < my_string 길이 < 100
+
+// 조건 & 결괏값
+const my_string = "Bcad";
+
+const answer = "abcd";
+
+// 정답
+// - 목적
+// 	- .toLowerCase() 메서드는 하나의 문자뿐만 아니라 문자열에도 기능함.
+function solution(my_string) {
+  return [...my_string.toLowerCase()].sort().join("");
+
+  //     return [...my_string].sort((a, b) => {
+  //         if(a.toLowerCase() < b.toLowerCase()) {
+  //             return -1;
+  //         }
+  //         else if(a.toLowerCase() === b.toLowerCase()) {
+  //             return 0;
+  //         }
+  //         else {
+  //             return 1;
+  //         }
+  //     }).reduce((acc, cur) => acc + (cur.toLowerCase() === cur ? cur : cur.toLowerCase()), '');
+
+  //     return my_string.replace(/[A-Z]/g, item => item.toLowerCase()).split('').sort().join('');
+}
+
+230719;
+function solution230719(my_string) {
+  return;
+}
+
+console.log(solution230719(my_string) === answer);
